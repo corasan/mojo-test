b944b883a16698eec16b0cb1e990cc4f044f9f39
Henry Paulino <henrypl360@gmail.com>
2021-11-22 09:46:03 +0000

    add space

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -40,8 +40,9 @@ const Notification = ({ description, shown, onClose, onOpen }: NotificationProps
       <View style={styles.notification}>
         <Text style={styles.description}>
           {description}
+          {'  '}
           <TouchableOpacity onPress={onOpen} accessibilityLabel="Open Mojo's daily poll">
-            <Text style={styles.open}> Open</Text>
+            <Text style={styles.open}>Open</Text>
           </TouchableOpacity>
         </Text>
         <TouchableOpacity
